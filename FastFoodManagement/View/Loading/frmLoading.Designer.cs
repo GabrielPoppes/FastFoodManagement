b7a69f682e6ce21e4d64da5bd3ddd02419fbf4db
Gabriel Poppes <gabrielpoppes@outlook.com>
2021-09-21 10:34:02 -0300

    Adicionado logotipo na form frmLoading e na frmSystemOptions

--- FastFoodManagement/View/Loading/frmLoading.Designer.cs
+++ FastFoodManagement/View/Loading/frmLoading.Designer.cs
@@ -31,15 +31,18 @@ namespace FastFoodManagement
         {
             this.components = new System.ComponentModel.Container();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.panelBarProgressMain = new System.Windows.Forms.Panel();
             this.panelBarProgressLoading = new System.Windows.Forms.Panel();
+            this.panelBarProgressMain = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Controls.Add(this.panelBarProgressLoading);
             this.panel1.Controls.Add(this.panelBarProgressMain);
             this.panel1.Controls.Add(this.label1);
@@ -50,6 +53,23 @@ namespace FastFoodManagement
             this.panel1.Size = new System.Drawing.Size(684, 361);
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panelBarProgressLoading
+            // 
+            this.panelBarProgressLoading.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(109)))), ((int)(((byte)(137)))));
+            this.panelBarProgressLoading.Location = new System.Drawing.Point(0, 332);
+            this.panelBarProgressLoading.Name = "panelBarProgressLoading";
+            this.panelBarProgressLoading.Size = new System.Drawing.Size(30, 29);
+            this.panelBarProgressLoading.TabIndex = 0;
+            // 
+            // panelBarProgressMain
+            // 
+            this.panelBarProgressMain.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(57)))), ((int)(((byte)(79)))));
+            this.panelBarProgressMain.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelBarProgressMain.Location = new System.Drawing.Point(0, 332);
+            this.panelBarProgressMain.Name = "panelBarProgressMain";
+            this.panelBarProgressMain.Size = new System.Drawing.Size(684, 29);
+            this.panelBarProgressMain.TabIndex = 1;
             // 
             // label1
             // 
@@ -63,28 +83,22 @@ namespace FastFoodManagement
             this.label1.TabIndex = 0;
             this.label1.Text = "Carregando";
             // 
-            // panelBarProgressMain
-            // 
-            this.panelBarProgressMain.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(57)))), ((int)(((byte)(79)))));
-            this.panelBarProgressMain.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panelBarProgressMain.Location = new System.Drawing.Point(0, 332);
-            this.panelBarProgressMain.Name = "panelBarProgressMain";
-            this.panelBarProgressMain.Size = new System.Drawing.Size(684, 29);
-            this.panelBarProgressMain.TabIndex = 1;
-            // 
-            // panelBarProgressLoading
-            // 
-            this.panelBarProgressLoading.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(109)))), ((int)(((byte)(137)))));
-            this.panelBarProgressLoading.Location = new System.Drawing.Point(0, 332);
-            this.panelBarProgressLoading.Name = "panelBarProgressLoading";
-            this.panelBarProgressLoading.Size = new System.Drawing.Size(30, 29);
-            this.panelBarProgressLoading.TabIndex = 0;
-            // 
             // timer1
             // 
             this.timer1.Enabled = true;
             this.timer1.Interval = 15;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::FastFoodManagement.Properties.Resources.GERENCIADORLOGO;
+            this.pictureBox1.Location = new System.Drawing.Point(118, 101);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(461, 141);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
             // 
             // Loading
             // 
@@ -99,6 +113,7 @@ namespace FastFoodManagement
             this.Text = "Carregando";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -110,6 +125,7 @@ namespace FastFoodManagement
         private System.Windows.Forms.Panel panelBarProgressMain;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- FastFoodManagement/View/SystemOptions/frmSystemOptions.Designer.cs
+++ FastFoodManagement/View/SystemOptions/frmSystemOptions.Designer.cs
@@ -32,12 +32,15 @@ namespace FastFoodManagement.View.SystemOptions
             this.panelfrmSystemOptions = new System.Windows.Forms.Panel();
             this.btnCaixa = new System.Windows.Forms.Button();
             this.btnPedidos = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panelfrmSystemOptions.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panelfrmSystemOptions
             // 
             this.panelfrmSystemOptions.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(13)))), ((int)(((byte)(19)))));
+            this.panelfrmSystemOptions.Controls.Add(this.pictureBox1);
             this.panelfrmSystemOptions.Controls.Add(this.btnCaixa);
             this.panelfrmSystemOptions.Controls.Add(this.btnPedidos);
             this.panelfrmSystemOptions.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -51,11 +54,11 @@ namespace FastFoodManagement.View.SystemOptions
             // 
             this.btnCaixa.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(51)))), ((int)(((byte)(102)))));
             this.btnCaixa.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnCaixa.Font = new System.Drawing.Font("Microsoft YaHei UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCaixa.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCaixa.ForeColor = System.Drawing.Color.White;
-            this.btnCaixa.Location = new System.Drawing.Point(138, 132);
+            this.btnCaixa.Location = new System.Drawing.Point(201, 194);
             this.btnCaixa.Name = "btnCaixa";
-            this.btnCaixa.Size = new System.Drawing.Size(187, 93);
+            this.btnCaixa.Size = new System.Drawing.Size(123, 58);
             this.btnCaixa.TabIndex = 1;
             this.btnCaixa.Text = "Caixa";
             this.btnCaixa.UseVisualStyleBackColor = false;
@@ -64,14 +67,25 @@ namespace FastFoodManagement.View.SystemOptions
             // 
             this.btnPedidos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(51)))), ((int)(((byte)(102)))));
             this.btnPedidos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnPedidos.Font = new System.Drawing.Font("Microsoft YaHei UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPedidos.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnPedidos.ForeColor = System.Drawing.Color.White;
-            this.btnPedidos.Location = new System.Drawing.Point(367, 132);
+            this.btnPedidos.Location = new System.Drawing.Point(370, 194);
             this.btnPedidos.Name = "btnPedidos";
-            this.btnPedidos.Size = new System.Drawing.Size(187, 93);
+            this.btnPedidos.Size = new System.Drawing.Size(123, 58);
             this.btnPedidos.TabIndex = 0;
             this.btnPedidos.Text = "Pedidos";
             this.btnPedidos.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::FastFoodManagement.Properties.Resources.GERENCIADORLOGO;
+            this.pictureBox1.Location = new System.Drawing.Point(166, 79);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(369, 82);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
             // 
             // frmSystemOptions
             // 
@@ -84,6 +98,7 @@ namespace FastFoodManagement.View.SystemOptions
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Opções de setores";
             this.panelfrmSystemOptions.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -93,5 +108,6 @@ namespace FastFoodManagement.View.SystemOptions
         private System.Windows.Forms.Panel panelfrmSystemOptions;
         private System.Windows.Forms.Button btnCaixa;
         private System.Windows.Forms.Button btnPedidos;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
